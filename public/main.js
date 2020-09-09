472049ec4a4abf10cca5bc033075bf4413247b37
iwinter74 <isabelle.winternazlisoz@googlemail.com>
2020-09-09 16:57:24 +0200

    v.0.1.1

---
+++ public/main.js
@@ -0,0 +1,13 @@
+
+
+function myActive() {
+    let all = document.getElementById("all")
+    let newArt = document.getElementById("new")
+    let blog = document.getElementById("blog")
+    all.classList.toggle("active")
+    newArt.classList.toggle("active")
+    blog.classList.toggle("active")
+
+}
+
+
